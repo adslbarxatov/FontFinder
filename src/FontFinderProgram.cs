1066978d4d033b6caf7fe4cc0af3a00aaff1b612
RD AAOW Free development lab <adslbarxatov@gmail.com>
2026-04-20 16:46:18 +1100

    Assembly v 4.0.4 (20.04.26)

--- src/FontFinderProgram.cs
+++ src/FontFinderProgram.cs
@@ -20,7 +20,7 @@ namespace RD_AAOW
 			RDLocale.InitEncodings ();
 
 			// Язык интерфейса и контроль XPUN
-			if (!RDLocale.IsXPUNClassAcceptable)
+			if (!RDLocale.CheckXPUNClass ())
 				return;
 
 			// Проверка запуска единственной копии
